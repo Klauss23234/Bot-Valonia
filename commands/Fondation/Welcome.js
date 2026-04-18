7c0e26ec7c3b12b9769398c6523d18d9b00bc400
Klauss23234 <alexdoneux007@gmail.com>
2026-04-19 01:51:10 +0200

    Delete commands/Fondation/Welcome.js

--- commands/Fondation/Welcome.js
+++
@@ -1,9 +0,0 @@
-export const welcomeEvent = {
-  name: 'guildMemberAdd',
-  execute(member) {
-    const channel = member.guild.systemChannel;
-    if (!channel) return;
-
-    channel.send(`Bienvenue ${member}`);
-  }
-};
